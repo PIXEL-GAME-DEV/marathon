e7a5b9ca672b51a94ee46e6b2f72f23f11cd8657
Gleb Sidorov <105152340+PIXEL-GAME-DEV@users.noreply.github.com>
2026-07-12 09:26:27 +1000

    separate weak and strong rumble

--- source/testing/test_rumble.gd
+++ source/testing/test_rumble.gd
@@ -1,8 +1,9 @@
 extends Control
 
 
-@onready var strength: HSlider = $CenterContainer/PanelContainer/VBoxContainer/HBoxContainer/Strength
+@onready var strong_magnitude: HSlider = $CenterContainer/PanelContainer/VBoxContainer/HBoxContainer/StrongMagnitude
+@onready var weak_magnitude: HSlider = $CenterContainer/PanelContainer/VBoxContainer/HBoxContainer2/WeakMagnitude
 
 
 func _physics_process(delta: float) -> void:
-	Input.start_joy_vibration(0, strength.value, strength.value, delta)
+	Input.start_joy_vibration(0, weak_magnitude.value, strong_magnitude.value, delta)
